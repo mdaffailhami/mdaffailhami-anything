ba80632c588c06d499777d1f6d6df39acbfc3732
Daffa Ilhami <mdaffailhami@gmail.com>
2026-02-02 18:20:34 +0800

    fix: add missing title & fix weird article color

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,7 +7,10 @@
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<title>Daffa Ilhami | Anything</title>
+</svelte:head>
 
 <SvelteTheme enableSystem enableColorScheme defaultTheme="system" attribute="class">
 	{@render children()}
